19679245569b823a5b8677d7ccbed664540f370e
William Gollinger <wgolling@gmail.com>
2021-02-03 18:54:23 -0500

    test Checklist constructor

--- app.py
+++ app.py
@@ -29,8 +29,39 @@ def add_dicts(d1, d2, multiplier=1):
 class Checklist():
 
     def __init__(self, recipes_dict):
-        self.recipes = dict(recipes_dict)
+        # Deep copy and validate input dict at the same time.
+        Checklist._validate_dict(recipes_dict)
+        self.recipes = dict()
+        for recipe_name, recipe in recipes_dict.items():
+            recipe_name = Checklist._validate_string_key(recipe_name)
+            Checklist._validate_dict(recipe)
+            self.recipes[recipe_name] = dict()
+            for ingredient, amount in recipe.items():
+                ingredient = Checklist._validate_string_key(ingredient)
+                amount = Checklist._validate_int_value(amount)
+                self.recipes[recipe_name][ingredient] = amount
         Checklist._flatten_recipes_to_ingredients(self.recipes)
+
+    @staticmethod
+    def _validate_dict(input_):
+        if not isinstance(input_, dict):
+            raise ValueError("Expected dictionary.")
+
+    @staticmethod
+    def _validate_string_key(input_):
+        if not isinstance(input_, str):
+            raise ValueError("All keys must be strings.")
+        return str(input_)
+
+    @staticmethod
+    def _validate_int_value(input_):
+        result = None
+        try:
+            result = int(input_)
+        except ValueError as e:
+            raise ValueError("All ingredient quantities must be integers.") from e
+        return result
+
 
     @staticmethod
     def _flatten_recipes_to_ingredients(data):
--- app_test.py
+++ app_test.py
@@ -15,3 +15,33 @@ class TestChecklist(unittest.TestCase):
 
     def test_constructor(self):
         assert(self.chkl.recipes == TestChecklist.TEST_RECIPES)
+        assert(self.chkl.recipes is not TestChecklist.TEST_RECIPES)
+        string_int = {
+            "Buh": {"Ing": "2"}
+        }
+        d = Checklist(string_int)
+        assert(d.recipes["Buh"]["Ing"] == 2)
+
+    def test_bad_constructor_input(self):
+        not_a_dict = "foo"
+        self.assertRaises(ValueError, Checklist, not_a_dict)
+
+        invalid_key = {
+            2: {"Ing1": 2}
+        }
+        self.assertRaises(ValueError, Checklist, invalid_key)
+
+        invalid_recipe = {
+            "bad_recipe": 5
+        }
+        self.assertRaises(ValueError, Checklist, invalid_recipe)
+
+        invalid_ingredient = {
+            "Foo": {2: 3}
+        }
+        self.assertRaises(ValueError, Checklist, invalid_ingredient)
+
+        invalid_amount = {
+            "Foo": {"ing": "not an int"}
+        }
+        self.assertRaises(ValueError, Checklist, invalid_amount)
