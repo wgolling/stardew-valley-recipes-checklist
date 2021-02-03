a864eed496639adc128ab38c0e74ad72807bfe45
William Gollinger <wgolling@gmail.com>
2021-02-03 15:42:51 -0500

    add helper functions

--- app.py
+++ app.py
@@ -4,23 +4,37 @@ import json
 from pathlib import Path
 
 def run():
-    Checklist.cooking_checklist()
-    Checklist.crafting_checklist()
+    data_path = Path(".") / "data"
+    cook = Checklist(load_json(data_path / "cooking_recipes.json"))
+    cook.print_recipes()
+    crft = Checklist(load_json(data_path / "crafting_recipes.json"))
+    crft.print_recipes()
+
+def load_json(file_path):
+    data = None
+    with open(file_path) as f:
+        data = json.load(f)
+    return data
+
+def add_key_value_to_dict(k, v, d):
+    if k not in d:
+        d[k] = 0
+    d[k] += v
+
+def add_dicts(d1, d2, multiplier=1):
+    for k, v in d2.items():
+        add_key_value_to_dict(k, multiplier * v, d1)
 
 
 class Checklist():
 
-    def __init__(self, json_file_path):
-        with open(json_file_path) as f:
-            data = json.load(f)
-            print(data)
+    def __init__(self, recipes_dict):
+        self.recipes = dict(recipes_dict)
+        Checklist._flatten_recipes_to_ingredients(self.recipes)
 
     @staticmethod
-    def cooking_checklist():
-        path = Path(".") / "data" / "cooking_recipes.json"
-        return Checklist(path)
+    def _flatten_recipes_to_ingredients(data):
+        pass
 
-    @staticmethod
-    def crafting_checklist():
-        path = Path(".") / "data" / "crafting_recipes.json"
-        return Checklist(path)
+    def print_recipes(self):
+        print(self.recipes)
