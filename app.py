3e9ce3d861229e042081850f420b5ba68a7b9b00
William Gollinger <wgolling@gmail.com>
2021-02-03 00:10:37 -0500

    start Checklist definition; correctly load json

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
 """Main app file."""
 
+import json
+from pathlib import Path
+
 def run():
-    print("Hello!")
+    Checklist.cooking_checklist()
+    Checklist.crafting_checklist()
+
+
+class Checklist():
+
+    def __init__(self, json_file_path):
+        with open(json_file_path) as f:
+            data = json.load(f)
+            print(data)
+
+    @staticmethod
+    def cooking_checklist():
+        path = Path(".") / "data" / "cooking_recipes.json"
+        return Checklist(path)
+
+    @staticmethod
+    def crafting_checklist():
+        path = Path(".") / "data" / "crafting_recipes.json"
+        return Checklist(path)
