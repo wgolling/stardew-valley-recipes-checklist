48ab170d9003eefda3a66c104e0bb689f23a3dee
William Gollinger <wgolling@gmail.com>
2021-02-03 23:19:26 -0500

    cleanup and documentation

--- app.py
+++ app.py
@@ -27,38 +27,68 @@ def add_dicts(d1, d2, multiplier=1):
 
 
 class Checklist():
+    """Manages a dictionary of recipes and a checklist of ingredients.
+    
+    Attributes:
+        recipes (dict): A dictionary representing a collection of recipes.
+        ingredients (dict): A dictionary of raw ingredients and the quantities
+            needed to complete all of the recipes.
+    
+    """
 
     def __init__(self, recipes_dict):
-        # Deep copy and validate input dictionary at the same time.
-        Checklist._validate_dict(recipes_dict)
-        self.recipes = dict()
-        for recipe_name, recipe in recipes_dict.items():
-            recipe_name = Checklist._validate_string_key(recipe_name)
-            Checklist._validate_dict(recipe)
-            self.recipes[recipe_name] = dict()
-            for ingredient, amount in recipe.items():
-                ingredient = Checklist._validate_string_key(ingredient)
-                amount = Checklist._validate_int_value(amount)
-                self.recipes[recipe_name][ingredient] = amount
-        # Construct ingredients dictionary.
+        """Construsts a Checklist object given a dictionary of recipes.
+        
+        Args:
+            recipes_dict (dict): A dictionary representing a collection of recipes.
+                The keys are recipes names as strings, and each value is itself a
+                dictionary, whose keys are ingredient name strings and whose values
+                are non-negative integers.
+        
+        """
+        self.recipes = Checklist._validate_input(recipes_dict)
         self.ingredients = Checklist._flatten_recipes_to_ingredients(self.recipes)
 
     @staticmethod
+    def _validate_input(recipes_dict):
+        """Deep copy and validate input dictionary at the same time."""
+        Checklist._validate_dict(recipes_dict)
+        result = dict()
+        for recipe_name, recipe in recipes_dict.items():
+            recipe_name = Checklist._validate_string_key(recipe_name)
+            result[recipe_name] = Checklist._validate_recipe(recipe)
+        return result
+
+    @staticmethod
+    def _validate_recipe(recipe):
+        """Checks that the keys are strings and the values are intable."""
+        Checklist._validate_dict(recipe)
+        result = dict()
+        for ingredient, amount in recipe.items():
+            ingredient  = Checklist._validate_string_key(ingredient)
+            amount      = Checklist._validate_int_value(amount)
+            result[ingredient] = amount
+        return result
+
+    @staticmethod
     def _validate_dict(input_):
+        """Raises ValueError if input is not a dictionary."""
         if not isinstance(input_, dict):
             raise ValueError("Expected dictionary.")
 
     @staticmethod
-    def _validate_string_key(input_):
-        if not isinstance(input_, str):
+    def _validate_string_key(key):
+        """Raises ValueError if the given key is not a string, otherwise returns a copy."""
+        if not isinstance(key, str):
             raise ValueError("All keys must be strings.")
-        return str(input_)
+        return str(key)
 
     @staticmethod
-    def _validate_int_value(input_):
+    def _validate_int_value(value):
+        """Raises ValueError if the given value is not convertible to int, otherwise returns an in copy."""
         result = None
         try:
-            result = int(input_)
+            result = int(value)
         except ValueError as e:
             raise ValueError("All ingredient quantities must be integers.") from e
         return result
