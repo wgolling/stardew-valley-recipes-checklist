2980863a6ed4e4868c16838893c05dbe9c26c098
William Gollinger <wgolling@gmail.com>
2021-02-05 04:12:47 -0500

    clean up frame structure

--- app.py
+++ app.py
@@ -23,30 +23,9 @@ def app_window(checklists):
     # crafting_ingredients_string = dict_to_string(crft.ingredients)
     
     # Frames
-    frm_recipes = tk.Frame(master=window, relief=tk.SUNKEN, borderwidth=3)
-    frm_recipes.grid(row=0, column=0, sticky="ns")
-    frm_ingredients = tk.Frame(master=window, relief=tk.SUNKEN, borderwidth=3)
-    frm_ingredients.grid(row=0, column=1, sticky="ns")
-
-    # Frame contents
-    cooking_recipes_frame = ScrollFrame(frm_recipes, cooking_recipes_string)
-    cooking_recipes_frame.pack(side="top", fill="both", expand=True)
-    cooking_ingredients_frame = ScrollFrame(frm_ingredients, cooking_ingredients_string)
-    cooking_ingredients_frame.pack(side="top", fill="both", expand=True)
-
-    # crafting_recipes = tk.Label(
-    #     master=frm_recipes,
-    #     text=crafting_recipes_string, 
-    #     fg="white",
-    #     bg="#34A2FE"
-    # )
-    # crafting_recipes.pack()
-    # crafting_ingredients = tk.Label(
-    #     master=frm_ingredients,
-    #     text=crafting_ingredients_string, 
-    #     fg="white",
-    #     bg="#34A2FE"
-    # )
-    # crafting_ingredients.pack()
+    cooking_recipes_frame = ScrollFrame(window, cooking_recipes_string)
+    cooking_recipes_frame.grid(row=0, column=0, sticky="ns")
+    cooking_ingredients_frame = ScrollFrame(window, cooking_ingredients_string)
+    cooking_ingredients_frame.grid(row=0, column=1, sticky="ns")
 
     window.mainloop()
