d012eef605aef53caa40eb9d0481a430847fc74d
William Gollinger <wgolling@gmail.com>
2021-02-05 01:33:53 -0500

    add tkinter window

--- app.py
+++ app.py
@@ -2,17 +2,35 @@
 
 import json
 from pathlib import Path
+import tkinter as tk
 
 from obj.checklist import Checklist
 
 def run():
     data_path = Path(".") / "data"
     cook = Checklist(load_json(data_path / "cooking_recipes.json"))
+    crft = Checklist(load_json(data_path / "crafting_recipes.json"))
+    app_window([cook, crft])
+
+def app_window(checklists):
+    window = tk.Tk()
+    cook, crft = checklists[0], checklists[1]
     cook.print_recipes()
     cook.print_ingredients()
-    crft = Checklist(load_json(data_path / "crafting_recipes.json"))
     crft.print_recipes()
     crft.print_ingredients()
+    
+    # Label
+    greeting = tk.Label(
+        text="Hello!", 
+        fg="white",
+        bg="#34A2FE",
+        width=10,
+        height=10
+    )
+    greeting.pack()
+
+    window.mainloop()
 
 def load_json(file_path):
     data = None
