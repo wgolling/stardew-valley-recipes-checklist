4e3532162e822e130cf22289e36c4641cad26077
William Gollinger <wgolling@gmail.com>
2021-02-21 22:33:50 -0500

    documentation and cleanup

--- obj/widgets.py
+++ obj/widgets.py
@@ -5,8 +5,29 @@ from functools import partial
 from lib.util import dict_to_string
 
 class ChecklistFrame(tk.Frame):
+    """A frame having a RecipesFrame and an IngredientsFrame as children.
+
+    Displays total ingredient requirements for a Checklist. Allows a user to
+    click buttons corresponding to recipes, which toggles their "completed"
+    state and refreshes displays.
+    
+    Attributes:
+        frm_recipes (:obj: `RecipesFrame`): A frame containing buttons for 
+            toggling recipes.
+        frm_ingredients (:obj: `IngredientsFrame`): A frame displaying ingredients.
+    
+    """
 
     def __init__(self, parent, checklist, row=0):
+        """Constructs a ChecklistFrame given a parent frame and a Checklist.
+
+        Args:
+            parent (:obj: `Frame`): The parent frame.
+            checklist (:obj: `Checklist`): The data structure which the GUI manipulates.
+            row (:obj: `int`, optional): The row that self occupies in its
+                parent's grid.
+
+        """
         tk.Frame.__init__(self, parent)
         
         self.frm_recipes = RecipesFrame(self, checklist)
@@ -15,14 +36,32 @@ class ChecklistFrame(tk.Frame):
         self.frm_ingredients.grid(row=row, column=1)
 
     def refresh_ingredients(self):
+        """Tells the ingredients frame to refresh its display."""
         self.frm_ingredients.refresh_ingredients()
 
 
-# Implements Brian Oakley's answer to the following StackOverflow question:
-# https://stackoverflow.com/questions/3085696/adding-a-scrollbar-to-a-group-of-widgets-in-tkinter
 class ScrollFrame(tk.Frame):
+    """
+    A general frame with a scrollbar on the right, populated by content.
+
+    Subclasses should override the populate method.
+
+    Implements Brian Oakley's answer to the following StackOverflow question:
+    https://stackoverflow.com/questions/3085696/adding-a-scrollbar-to-a-group-of-widgets-in-tkinter
+
+    """
 
     def __init__(self, parent, content):
+        """Constructs a ScrollFrame given a parent frame and general content.
+        
+        The constructor ends with a call to self.populate, a method which should
+        be overriden by subclasses.
+
+        Args:
+            parent (:obj: `Frame`): The parent frame.
+            content (:obj: `object`): The content of the ScrollFrame.
+
+        """
         tk.Frame.__init__(self, parent)
         self.canvas = tk.Canvas(self, borderwidth=0, background="#ffffff")
         self.frame = tk.Frame(self.canvas, background="#ffffff")
@@ -44,9 +83,10 @@ class ScrollFrame(tk.Frame):
         self.populate(content)
 
     def populate(self, content):
+        """Dummy populate method."""
         lbl_content = tk.Label(
             master=self.frame,
-            text=content, 
+            text=str(content), 
             fg="white",
             bg="#34A2FE"
         )
@@ -58,41 +98,50 @@ class ScrollFrame(tk.Frame):
 
 
 class RecipesFrame(ScrollFrame):
+    """A frame containing buttons corresponding to recipes in a given Checklist.
+    
+    Each button toggles the corresponding recipe's "completed" state in the Checklist.
 
-    def toggle(self, checklist, recipe):
-        checklist.toggle_recipe(recipe)
+    """
+
+    def populate(self, checklist):
+        """Populates the frame with recipe buttons."""
+        self.checklist = checklist
+        self.buttons = dict()
+        for name in checklist.recipes.keys():
+            toggle_recipe = partial(self.toggle, name)
+            btn_recipe = tk.Button(master=self.frame, command=toggle_recipe, text=name)
+            btn_recipe.pack()
+            self.buttons[name] = btn_recipe
+
+    def toggle(self, recipe):
+        """Toggles the recipe, refreshing the button and ingredients display."""
+        self.checklist.toggle_recipe(recipe)
         self.refresh_button(recipe)
         self.parent.refresh_ingredients()
 
-    def populate(self, checklist):
-        self.checklist = checklist
-        self.buttons = dict()
-        for k in checklist.recipes.keys():
-            name = str(k)
-            toggle_recipe = partial(self.toggle, checklist, name)
-            btn_recipe = tk.Button(master=self.frame, command=toggle_recipe, text=name)
-            btn_recipe.pack()
-            self.buttons[k] = btn_recipe
-
-    def refresh_button(self, recipe):        
+    def refresh_button(self, recipe): 
+        """Refreshes button display according to whether the recipe is complete."""
         button = self.buttons[recipe]
         text = ("X " + recipe) if recipe in self.checklist.completed else recipe
         button.configure(text=text)
 
 
 class IngredientsFrame(ScrollFrame):
+    """A frame displaying the remaining ingredients in the given Checklist."""
 
     def populate(self, checklist):
+        """Poluplates frame with ingredient requirements."""
         self.checklist = checklist
         ingredients_string = dict_to_string(self.checklist.ingredients)
         self.lbl_ingredients = tk.Label(
             master=self.frame,
-            text=ingredients_string, 
-            fg="white",
-            bg="#34A2FE"
+            text=ingredients_string,
+            bg="white"
         )
         self.lbl_ingredients.pack()
 
     def refresh_ingredients(self):
+        """Refreshes ingredients display."""
         ingredients_string = dict_to_string(self.checklist.ingredients)
         self.lbl_ingredients["text"] = ingredients_string
