d0b066ede8bba810dca4f8ac3e7ac9ba162b8f5f
William Gollinger <wgolling@gmail.com>
2021-02-22 02:18:18 -0500

    add dummy  menu with new, open, save, exit options

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import tkinter as tk
+from tkinter import filedialog
 
 from obj.checklist import Checklist
 from obj.widgets import ChecklistFrame
@@ -19,10 +20,31 @@ def app_window(checklists):
     crft = checklists[1]
     
     # Frames
+    def do_nothing():
+        print("Blah")
+
+    def load():
+        filename = filedialog.askopenfilename(
+                initialdir = Path(".") / "saves",
+                title = "Select file",
+                filetypes = (("checklist files","*.chk"),("all files","*.*"))
+        )        
+        print(filename)
+
+    menubar = tk.Menu(window)
+    filemenu = tk.Menu(menubar, tearoff=0)
+    filemenu.add_command(label="New", command=do_nothing)
+    filemenu.add_command(label="Open", command=load)
+    filemenu.add_command(label="Save", command=do_nothing)
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit", command=window.quit)    
+    menubar.add_cascade(label="File", menu=filemenu)
+
     frm_cooking = ChecklistFrame(window, cook)
     frm_cooking.grid(row=0, column=0)
 
     frm_crafting = ChecklistFrame(window, crft, row=1)
     frm_crafting.grid(row=1, column=0)
 
+    window.config(menu=menubar)
     window.mainloop()
